2bdb8021dae6b040e5665063595a6f16e2ed57e7
SHADOW11204 <tawasexcelindo@gmail.com>
2026-07-14 20:22:04 +0700

    restore

--- frontend/src/app/api/proxy/sicp-sync/test-public/route.js
+++
@@ -1,8 +0,0 @@
-// src/app/api/proxy/sicp-sync/test-public/route.js
-import { NextResponse } from 'next/server';
-
-export async function GET() {
-  const response = await fetch('https://sistem-skpi-production.up.railway.app/api/sicp-sync/test-public');
-  const data = await response.json();
-  return NextResponse.json(data);
-}
